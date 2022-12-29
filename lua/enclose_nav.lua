bdb579274373ed878a77c6d213acfb312b906703
Bartek Winter <bartekwinter3@gmail.com>
2022-12-29 16:34:27 +0000

    Added enclose_nav.lua script

---
+++ lua/enclose_nav.lua
@@ -0,0 +1,31 @@
+ENCLOSING_SYMBOL = {")", "]", "\'", "\"", "}", ">"}
+
+function inside_list(list, value) 
+	for _, v in ipairs(list) do
+		if v == value then
+			return true
+		end
+	end
+	return false
+end
+
+function enclose_nav()
+	-- get the current cursor position
+	local cursor_pos = vim.api.nvim_win_get_cursor(0);
+	local line = vim.api.nvim_buf_get_lines(0, cursor_pos[1]-1, cursor_pos[1], false)[1]
+	-- flag indicating if we are inside enclosing parens or brackets, or other supported symbol
+	-- iterate over the characters in the line
+	for i = cursor_pos[2] + 1, #line do
+		local char = line:sub(i, i)
+		if inside_list(ENCLOSING_SYMBOL, char) then
+			-- move the cursor to the position of the closing token 
+			vim.api.nvim_win_set_cursor(0, {cursor_pos[1], i - 1})
+			break
+		end
+	end
+end
+
+function setup()
+	-- Set the keybinding for the enclose_nav function in insert mode
+	vim.api.nvim_set_keymap("i", "<C-e>", "<ESC>:lua enclose_nav()<CR>a", {noremap = true})
+end
